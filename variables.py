46dad61ee4b38be4ad35087d3db900f2d9f8a3a9
KinjalDaftary <44295690+KinjalDaftary@users.noreply.github.com>
2019-01-24 17:20:48 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,28 @@
+"""
+Python Homework #1
+
+My first project on variable.
+Song Name: Ankh marey
+
+"""
+Song = "Aankh Maare"
+Movie = "Simmba"
+Director = "Rohit Shetty"
+Lyrics = "Shabbir Ahmed"
+Singer = "Neha Kakkar,Mika Singh,Kumar Sanu"
+Duration = "3:33"
+MaleActor = "Ranveer Singh"
+FemaleActor = "Sara Ali Khan"
+ReleasedDate = 2018
+OriginalSongFrom = 1996
+
+print(Song)
+print(Movie)
+print(Director)
+print(Lyrics)
+print(Singer)
+print(Duration)
+print(MaleActor)
+print(FemaleActor)
+print(ReleasedDate)
+print(OriginalSongFrom)
